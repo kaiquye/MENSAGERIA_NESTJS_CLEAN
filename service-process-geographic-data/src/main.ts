a439af2a902302da8b3898fa777c991f83c3c8af
kaiyque <kaiquemendesilva82@gmail.com>
2022-06-27 15:32:19 -0300

    🕶 : configurando modulos

--- service-process-geographic-data/src/main.ts
+++ service-process-geographic-data/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
+import {
+  ClientsModule,
+  MicroserviceOptions,
+  Transport,
+} from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  ConfigModule.forRoot({
+    isGlobal: true,
+  });
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: process.env.clientId_account,
+          brokers: [process.env.brokers],
+        },
+        consumer: {
+          groupId: process.env.groupId_account,
+          allowAutoTopicCreation: true,
+        },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
