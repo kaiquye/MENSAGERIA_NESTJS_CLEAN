91d56431b1265d341e1d1e3443637a259f0f177c
kaiyque <kaiquemendesilva82@gmail.com>
2022-06-27 13:25:49 -0300

    🎠�kafka : configurando instacia

--- main-application/src/modules/orders/orders.module.ts
+++ main-application/src/modules/orders/orders.module.ts
@@ -5,8 +5,27 @@ import { NewOrderUsecases } from './useCases/new-order.usecases';
 import { ExistsOrderUsecases } from './useCases/exists-order.usecases';
 import { OrderRepository } from './repository/order.repository';
 import { PrismaService } from '../../database/config.database';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: process.env.clientId_account,
+            brokers: [process.env.brokers],
+          },
+          consumer: {
+            groupId: process.env.groupId_account,
+            allowAutoTopicCreation: true,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [
     OrdersService,
