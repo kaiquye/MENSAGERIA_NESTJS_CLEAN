c3e4b696a33d699e9480317915f7728f260f1c72
kaiyque <kaiquemendesilva82@gmail.com>
2022-06-27 11:39:13 -0300

    📀 : configurando pipes

--- main-application/src/main.ts
+++ main-application/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  ConfigModule.forRoot({
+    isGlobal: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
