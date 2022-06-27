a1ef5365c90c18ac95657f7726f183140c3f1aef
kaiyque <kaiquemendesilva82@gmail.com>
2022-06-27 11:11:02 -0300

    👨🏼‍🔧 : configurando validações DTO

--- main-application/src/modules/orders/dto/create-order.dto.ts
+++ main-application/src/modules/orders/dto/create-order.dto.ts
@@ -1,1 +1,38 @@
-export class CreateOrderDto {}
+import { INewOrder } from '../types/new-order.type';
+import {
+  IsString,
+  IsInt,
+  IsNumber,
+  IsPhoneNumber,
+  IsEmail,
+  isEmpty,
+  IsEmpty,
+  IsNotEmpty,
+  maxLength,
+  minLength,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateOrderDto implements INewOrder {
+  @IsString()
+  city: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  name: string;
+  @IsString()
+  neighborhood: string;
+  @IsString()
+  @MaxLength(15)
+  @MinLength(8)
+  phone: string;
+  @IsString()
+  street: string;
+  @IsString()
+  @MaxLength(2)
+  @MinLength(2)
+  uf: string;
+  @IsString()
+  zip_code: string;
+}
--- main-application/src/modules/orders/orders.service.ts
+++ main-application/src/modules/orders/orders.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { OrderRepository } from './repository/order.repository';
 
 @Injectable()
 export class OrdersService {
+  constructor(private repository: OrderRepository) {}
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    this.repository.registerNewOrder();
   }
 }
